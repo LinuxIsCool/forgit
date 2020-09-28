57548d98ce4f3c6d7388f800f3b446eed6e4f4d7
Shawn Anderson <shawnltf@pm.me>
2020-09-28 14:14:02 -0700

    Initialize omf plugin.

---
+++ init.fish
@@ -0,0 +1,6 @@
+# forgit initialization hook
+#
+# You can use the following variables in this file:
+# * $package       package name
+# * $path          package path
+# * $dependencies  package dependencies
